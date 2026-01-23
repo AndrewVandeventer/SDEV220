e7474cff4d7c6dcdca1c5e9aeed55fda938b63fb
Andrew Vandeventer <andrewwyattvandeventer@gmail.com>
2026-01-23 16:30:51 -0500

    Added module 2 assignments from class

---
+++ Module_2/M2Lab.py
@@ -0,0 +1,21 @@
+DEANS_LIST: float = 3.5
+DEANS_LIST_MSG: str = "You made the Dean's List!"
+HONOR_ROLL: float = 3.25
+HONOR_ROLL_MSG: str = "You made the Honor Roll!"
+SENTINEL: str = "ZZZ"
+INPUT_PROMPT1: str = 'Enter the first name (ZZZ to quit):'
+INPUT_PROMPT2: str = 'Enter the GPA: '
+
+first_name: str = ''
+gpa: float = 0.0
+
+while True:
+    first_name = input(INPUT_PROMPT1)
+    if first_name.upper() != SENTINEL:
+        gpa = float(input(INPUT_PROMPT2))
+        if gpa >= DEANS_LIST:
+            print(DEANS_LIST_MSG)
+    elif gpa >= HONOR_ROLL:
+            print(HONOR_ROLL_MSG)
+    else: 
+        break
